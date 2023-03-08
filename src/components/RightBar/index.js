34acd9bb9472152d6a07dd5e00e992f13569722c
shahriarbjit <shahriar.mehedi@bjitgroup.com>
2023-03-08 11:52:39 +0600

    modal add

--- src/components/RightBar/index.js
+++ src/components/RightBar/index.js
@@ -6,14 +6,69 @@ import { sidebarLinks } from "./data/sidebarLink";
 
 const RightBar = () => {
     const [active, setActive] = useState(0);
+    const [isOpen, setIsOpen] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleOpenModal = () => {
+      setIsOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsOpen(false);
+    };
+  
+    const handleUsernameChange = (event) => {
+      setUsername(event.target.value);
+    };
+  
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Handle form submission here
+      setUsername('');
+      setPassword('');
+      handleCloseModal();
+    };
     return (
         <div className="right-bar">
             <div className="right-bar__moon-icon">
                 <GiMoon />
             </div>
-            <div className="right-bar__login-icon" title="login">
+            <div className="right-bar__login-icon" onClick={handleOpenModal} title="login">
                 <BiLogIn />
             </div>
+            {isOpen && (
+        <div className="modal-overlay" onClick={handleCloseModal}>
+          <div className="modal-content" onClick={(event) => event.stopPropagation()}>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      )}
             <div className="right-bar__link-div">
                 {sidebarLinks().map((data, i) => (
                     <div className="right-bar__link-item" key={i}>
