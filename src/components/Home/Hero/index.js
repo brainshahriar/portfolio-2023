4d208aa1570d2fae54e9b0d19d086faebaaec679
shahriarbjit <shahriar.mehedi@bjitgroup.com>
2023-05-31 00:43:04 +0600

    updated

--- src/components/Home/Hero/index.js
+++ src/components/Home/Hero/index.js
@@ -5,47 +5,65 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import Typewriter from "typewriter-effect";
 import { AnalyticsEvent } from "../../../utils.js/gaEvent";
 
+import Hotjar from "@hotjar/browser";
 
-const HeroTop= () => {
+const siteId = 3513762;
+const hotjarVersion = 6;
 
-    const handleEventChange = (tech) => {
-        AnalyticsEvent('button_name', 'Hire_me', tech);
-    }
+Hotjar.init(siteId, hotjarVersion);
 
-    return (
-        <div className="hero-section">
-                <div className="hero-section__profile-info">
-                    <p className="hero-section__profile-title">
-                        I'm Al Shahriar <br />
-                        <span className="hero-section__profile-title--highlight">
-                            <Typewriter
-                                options={{
-                                    strings: ["Front-end Developer"],
-                                    autoStart: true,
-                                    loop: true,
-                                }}
-                            />
-                        </span>
-                    </p>
-                    <p className="hero-section__profile-description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Et, volutpat feugiat placerat lobortis. Natoque rutrum
-                        semper sed suspendisse nunc lectus.
-                    </p>
-                    <button className="hero-section__hire-btn" onClick={handleEventChange('Clicked')}>
-                        HIRE ME
-                        <span className="hero-section__hire-btn__icon">
-                            <AiOutlineArrowRight />
-                        </span>
-                    </button>
-                </div>
-                <img
-                    className="hero-section__profile-img"
-                    src={profileImg}
-                    alt=""
-                />
-            </div>
-    );
+// Initializing with `debug` option:
+Hotjar.init(siteId, hotjarVersion, {
+  debug: true,
+});
+
+const userId = "abc_123";
+const firstName = "shahriar";
+const favoriteColor = "blue";
+
+Hotjar.identify(userId, {
+  first_name: firstName,
+  color: favoriteColor,
+});
+
+const HeroTop = () => {
+  const handleEventChange = (tech) => {
+    AnalyticsEvent("button_name", "Hire_me", tech);
+  };
+
+  return (
+    <div className="hero-section">
+      <div className="hero-section__profile-info">
+        <p className="hero-section__profile-title">
+          I'm Al Shahriar <br />
+          <span className="hero-section__profile-title--highlight">
+            <Typewriter
+              options={{
+                strings: ["Front-end Developer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </p>
+        <p className="hero-section__profile-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat
+          feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc
+          lectus.
+        </p>
+        <button
+          className="hero-section__hire-btn"
+          onClick={handleEventChange("Clicked")}
+        >
+          HIRE ME
+          <span className="hero-section__hire-btn__icon">
+            <AiOutlineArrowRight />
+          </span>
+        </button>
+      </div>
+      <img className="hero-section__profile-img" src={profileImg} alt="" />
+    </div>
+  );
 };
 
 export default HeroTop;
