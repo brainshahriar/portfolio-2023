1a5057970d7b758dbce2a353b3ce203e06695b2e
shahriar.mehedi <shahriar.mehedi@bjitgroup.com>
2023-05-31 13:01:13 +0600

    d

--- src/utils.js/gaEvent.js
+++ src/utils.js/gaEvent.js
@@ -1,6 +1,7 @@
 import ReactGA from "react-ga4";
 
 export const AnalyticsEvent = (category, eventName, label) => {
+  console.log(category, eventName, label);
   ReactGA.event({
     category: category,
     action: eventName,
